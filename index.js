d8da07d5a8bd6c06c48755cd8c909e0075a55de5
nw1n <your@example.com>
2024-05-21 20:57:19 +0200

    remove singleton

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
 
 // JS DOC for Sqliter class, describe the getInstace method
 
@@ -7,123 +7,111 @@ import sqlite3 from 'sqlite3';
  * @description Sqliter class to interact with SQLite database
  */
 export default class Sqliter {
-    static instance = null;
-    db = null;
-    dbFilePath = null;
+  db = null;
+  dbFilePath = null;
 
-    /**
-     * @returns {Sqliter} Sqliter instance
-     */
-    static getInstance() {
-        if (Sqliter.instance == null) {
-            Sqliter.instance = new Sqliter();
-        }
-        return this.instance;
-    }
+  async init(dbFilePath) {
+    this.dbFilePath = dbFilePath;
+    this.db = await openDatabase(dbFilePath);
+    return this;
+  }
 
-    async init(dbFilePath) {
-        this.dbFilePath = dbFilePath;
-        this.db = await openDatabase(dbFilePath);
-        return Sqliter.getInstance();
-    }
+  async all(sql, params = []) {
+    console.log(this.db);
+    return await all(this.db, sql, params);
+  }
 
-    async all(sql, params = []) {
-        console.log(this.db);
-        return await all(this.db, sql, params);
-    }
+  async get(sql, params = []) {
+    return await get(this.db, sql, params);
+  }
 
-    async get(sql, params = []) {
-        return await get(this.db, sql, params);
-    }
+  async run(sql, params = []) {
+    return await run(this.db, sql, params);
+  }
 
-    async run(sql, params = []) {
-        return await run(this.db, sql, params);
-    }
+  async map(sql, params = []) {
+    return await map(this.db, sql, params);
+  }
 
-    async map(sql, params = []) {
-        return await map(this.db, sql, params);
-    }
-
-    async close() {
-        return await close(this.db);
-    }
+  async close() {
+    return await close(this.db);
+  }
 }
 
 // Function to open the database
 function openDatabase(filename) {
-    return new Promise((resolve, reject) => {
-        const db = new sqlite3.Database(filename, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(db);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(filename, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(db);
+      }
     });
+  });
 }
 
 // Function to run a SQL query and retrieve all rows
 function all(db, sql, params = []) {
-    return new Promise((resolve, reject) => {
-        db.all(sql, params, (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
     });
+  });
 }
 
 // Same as all, but returns rows as objects with key-value pairs
 function map(db, sql, params = [], callback) {
-    return new Promise((resolve, reject) => {
-        db.all(sql, params, (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
     });
+  });
 }
-
 
 // Function to run a SQL query and retrieve a single row
 function get(db, sql, params = []) {
-    return new Promise((resolve, reject) => {
-        db.get(sql, params, (err, row) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(row);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
     });
+  });
 }
 
 // Function to run a SQL query that doesn't return data (e.g., INSERT, UPDATE, DELETE)
 function run(db, sql, params = []) {
-    return new Promise((resolve, reject) => {
-        db.run(sql, params, function (err) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ lastID: this.lastID, changes: this.changes });
-            }
-        });
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ lastID: this.lastID, changes: this.changes });
+      }
     });
+  });
 }
 
 // Function to close the database
 function close(db) {
-    return new Promise((resolve, reject) => {
-        db.close((err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
+  return new Promise((resolve, reject) => {
+    db.close((err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
     });
+  });
 }
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import path from 'path';
-import Sqliter from './index.js';
+import sqliter from './index.js';
 
 const dbFilePath = path.resolve(dirname(fileURLToPath(import.meta.url)), 'test.db');
 
@@ -9,7 +9,7 @@ const dbFilePath = path.resolve(dirname(fileURLToPath(import.meta.url)), 'test.d
 main()
 
 async function main() {
-    const db = Sqliter.getInstance();
+    const db = new sqliter();
     await db.init(dbFilePath);
     await db.run('CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)');
     await db.run('INSERT INTO test (name) VALUES (?)', ['test']);
